1ea7eb6ddb2ff8d02106e2c697715199a38090d7
gechderib <gechderib@gmail.com>
2022-12-01 23:55:17 +0300

    connecting to api

--- src/features/posts/PostReaction.js
+++ src/features/posts/PostReaction.js
@@ -2,22 +2,28 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { reactionAdded } from "./postsSlice";
 
-const PostReaction = ({post}) => {
+const PostReaction = ({ post }) => {
   const dispatch = useDispatch();
   const reactions = {
-    thumbsup:"TT",
-    wow: "ww",
+    thumbsup: "TT",
+    wow: "WW",
     heart: "HH",
     rocket: "RR",
     coffee: "CC",
   };
-  return <div>
-    <button className="m-2" onClick={()=>dispatch(reactionAdded({postId:post.id,reaction:"thumbsup"}))}>TT {post.reactions.thumbsup}</button>
-    <button className="m-2" onClick={()=>dispatch(reactionAdded({postId:post.id,reaction:"wow"}))}>WW {post.reactions.wow}</button>
-    <button className="m-2" onClick={()=>dispatch(reactionAdded({postId:post.id, reaction:"heart"}))}>HH {post.reactions.heart}</button>
-    <button className="m-2" onClick={()=>dispatch(reactionAdded({postId:post.id, reaction:"rocket"}))}>RR {post.reactions.rocket}</button>
-    <button className="m-2" onClick={()=>dispatch(reactionAdded({postId:post.id, reaction:"coffee"}))}>CC {post.reactions.coffee}</button>
-  </div>;
+  const reactionButton = Object.entries(reactions).map(([name, emoji]) => {
+    return (
+      <button
+        className="m-2"
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+  return <div>{reactionButton}</div>;
 };
 
 export default PostReaction;
